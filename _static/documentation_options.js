2b45455b8f563c7375dd90f8103773d43008d3f8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-05-22 22:27:02 +0000

    Deploying to gh-pages from @ ArtifactDB/gobbler-py@86ba1a35a1b496640e940e91ef18b0405f0ddc01 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+ged29d42',
+    VERSION: '0.0.post1.dev1+g86ba1a3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
